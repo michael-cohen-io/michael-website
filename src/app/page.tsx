5e14706217e5ab5586f82a08798d6503b6256454
Michael Cohen <michael.cohen@opensea.io>
2023-11-16 21:05:16 -0500

    Basic progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className={`mb-1 text-5xl font-semibold`}>Michael Cohen</h1>
+      <h1 className={`mb-1 text-8xl text-accent-color font-light`}>
+        Michael Cohen
+      </h1>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "accent-color": "#3b82f6",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
