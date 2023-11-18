649bf033d2b40f9a30f4b84912f16e937320b889
Michael Cohen <michael.cohen@opensea.io>
2023-11-18 03:41:04 -0500

    hide cursor

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -18,14 +18,22 @@ export default function Hero() {
       <PageHeader title="Hero" hidden />
       <div className="mt-8 flex max-w-screen-xl items-start justify-between w-full">
         <div className="z-10 w-full max-w-xl px-1 xl:px-0">
-          <Typist key={1} onTypingDone={onTypingDone}>
+          <Typist
+            cursor={{ hideWhenDone: true }}
+            key={1}
+            onTypingDone={onTypingDone}
+          >
             <h1 className="text-left font-light text-6xl text-slate-500">
               hello, world.
             </h1>
             <Typist.Delay ms={500} />
           </Typist>
           {typeCounter > 0 && (
-            <Typist key={2} onTypingDone={onTypingDone}>
+            <Typist
+              cursor={{ hideWhenDone: true }}
+              key={2}
+              onTypingDone={onTypingDone}
+            >
               <h2 className="text-left font-thin text-4xl">
                 My name is{" "}
                 <Link href="/about" className="font-bold text-accent-color">
@@ -37,7 +45,11 @@ export default function Hero() {
             </Typist>
           )}
           {typeCounter > 1 && (
-            <Typist key={2} onTypingDone={onTypingDone}>
+            <Typist
+              cursor={{ hideWhenDone: true }}
+              key={2}
+              onTypingDone={onTypingDone}
+            >
               <h2 className="text-left font-thin text-4xl">
                 I&apos;m a software engineer.
               </h2>
