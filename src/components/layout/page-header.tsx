5dd39fd2a720576f4689096a5d7168f56ae36a47
Michael Cohen <michael.cohen@opensea.io>
2023-11-18 12:44:19 -0500

    change header middle

--- src/components/layout/page-header.tsx
+++ src/components/layout/page-header.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { capitalize } from "@/lib/utils";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function PageHeader({
   title,
@@ -11,26 +9,18 @@ export default function PageHeader({
   title?: string;
   hidden?: boolean;
 }) {
-  const pathname = usePathname();
-  const isActive = pathname === title;
   return title ? (
     <>
       <div
-        className={`mx-5 flex max-w-screen-xl items-start justify-between w-full mt-20 ${
+        className={`mx-5 flex max-w-screen-xl items-center justify-between w-full mt-20 ${
           hidden ? "invisible" : ""
         }`}
       >
         <div
-          className={`z-10 w-full max-w-lg px-1 xl:px-0 ${
-            hidden ? "invisible" : ""
-          }`}
+          className={`z-10 w-full px-1 xl:px-0 ${hidden ? "invisible" : ""}`}
         >
-          <h1 className="text-slate-500 font-light text-2xl">
-            {isActive ? (
-              capitalize(title)
-            ) : (
-              <Link href={`/${title}`}>{capitalize(title)}</Link>
-            )}
+          <h1 className="text-slate-500 text-center font-light text-2xl">
+            {capitalize(title)}
           </h1>
         </div>
       </div>
