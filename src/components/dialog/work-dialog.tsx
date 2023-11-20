638e364f2f4af2b85237a5defc7c29dc8dbcae5a
Michael Cohen <micohen13@gmail.com>
2023-11-20 04:29:44 -0500

    fix things

--- src/components/dialog/work-dialog.tsx
+++ src/components/dialog/work-dialog.tsx
@@ -9,20 +9,14 @@ import { Flex, Text } from "@radix-ui/themes";
 
 import Heading from "../typography/heading";
 
-const TextWithLineBreaks = ({
-  text,
-  textStyle,
-}: {
-  text: string;
-  textStyle: string;
-}) => {
+const TextWithLineBreaks = ({ text }: { text: string }) => {
   const lines = text.split("\\n");
 
   return (
     <>
       {lines.map((line, index) => (
         <>
-          <Text key={index} className={`${textStyle}`}>
+          <Text as="p" className="text-sm" key={index} size="1" weight="light">
             {line}
           </Text>
           <br />
@@ -51,13 +45,17 @@ export default function WorkDialog({
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="DialogOverlay">
-          <Dialog.Content className="DialogContent bg-slate-50 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[80vh] w-[90vw] max-w-lg translate-x-[-50%] translate-y-[-50%] rounded-lg pt-4 px-4 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <Dialog.Content className="DialogContent rounded-6 bg-slate-50 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[80vh] w-[90vw] max-w-lg translate-x-[-50%] translate-y-[-50%] rounded-lg pt-4 px-4 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
             <Dialog.Title className="text-mauve12 m-0">
               <Flex direction="column">
-                <Heading size="4">{workItem.company.name}</Heading>
+                <Heading size="4" as="h1">
+                  {workItem.company.name}
+                </Heading>
                 <Flex className="flex mb-4 items-center justify-between w-full">
-                  <Heading size="3">{workItem.role}</Heading>
-                  <Heading size="2">
+                  <Heading size="3" as="h2">
+                    {workItem.role}
+                  </Heading>
+                  <Heading size="2" as="h2">
                     {formatDate(workItem.startDate)} -{" "}
                     {formatDate(workItem.endDate)}
                   </Heading>
@@ -65,10 +63,7 @@ export default function WorkDialog({
               </Flex>
             </Dialog.Title>
             <Dialog.Description className="text-mauve11 mt-[10px] mb-5 leading-normal">
-              <TextWithLineBreaks
-                text={workItem.description}
-                textStyle="text-sm"
-              />
+              <TextWithLineBreaks text={workItem.description} />
             </Dialog.Description>
           </Dialog.Content>
         </Dialog.Overlay>
--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -10,6 +10,7 @@ import {
 import WorkDialog from "@/components/dialog/work-dialog";
 import IconByName from "@/components/icons/icons";
 import { formatDate } from "@/lib/utils";
+import { gray } from "@radix-ui/colors";
 
 function WorkItem({ workItem }: any) {
   const backgroundColor = workItem.iconColor || "#3b82f6";
@@ -30,6 +31,7 @@ function WorkItem({ workItem }: any) {
         boxShadow: "none",
         background: backgroundColor,
         color: "#fff",
+        borderRadius: "1.0rem",
       }}
       contentArrowStyle={{
         borderRight: `7px solid ${backgroundColor}`,
@@ -48,7 +50,7 @@ function WorkItem({ workItem }: any) {
 
 export default function WorkTimeline({ workData }: { workData: any[] }) {
   return (
-    <VerticalTimeline>
+    <VerticalTimeline lineColor={gray.gray6}>
       {workData.map((work: any) => (
         <WorkItem key={work.id} workItem={work} />
       ))}
