d2b91b70ae6e6d5a9288d9e5633a0675d1dde155
Michael Cohen <micohen13@gmail.com>
2023-11-20 10:10:13 -0500

    fix buttons

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -12,103 +12,101 @@ import { Code, Flex, Grid, Strong } from "@radix-ui/themes";
 import PageHeader from "../layout/page-header";
 import Heading from "../typography/heading";
 
-function TypingHero() {
+const firstTextBlock = (
+  <Code weight="bold" variant="soft" size="9">
+    hello, world.
+  </Code>
+);
+const secondTextBlock = (
+  <Heading className="text-left font-thin text-4xl ml-4">
+    My name is{" "}
+    <Link href="/about" className="text-accent-color">
+      <Strong>Michael Cohen</Strong>
+    </Link>
+    .
+  </Heading>
+);
+const thirdTextBlock = (
+  <Heading className="text-left font-thin text-4xl ml-4">
+    I&apos;m a software engineer.
+  </Heading>
+);
+
+const firstTypingBlock = (onTypingDone: () => void) => (
+  <>
+    <Typist
+      cursor={{ hideWhenDone: true, show: false }}
+      key={1}
+      onTypingDone={onTypingDone}
+      avgTypingDelay={35}
+    >
+      {firstTextBlock}
+      <Typist.Delay ms={500} />
+    </Typist>
+  </>
+);
+
+const secondTypingBlock = (typeCounter: number, onTypingDone: () => void) => (
+  <>
+    {typeCounter > 0 && (
+      <Typist
+        cursor={{ hideWhenDone: true, show: false }}
+        key={2}
+        onTypingDone={onTypingDone}
+        avgTypingDelay={35}
+      >
+        {secondTextBlock}
+        <Typist.Delay ms={500} />
+      </Typist>
+    )}
+  </>
+);
+
+const thirdTypingBlock = (typeCounter: number, onTypingDone: () => void) => (
+  <>
+    {typeCounter > 1 && (
+      <Typist
+        cursor={{ hideWhenDone: true, show: false }}
+        key={2}
+        onTypingDone={onTypingDone}
+        avgTypingDelay={35}
+      >
+        {thirdTextBlock}
+        <Typist.Delay ms={500} />
+      </Typist>
+    )}
+  </>
+);
+
+export default function Hero() {
   const [typeCounter, setTypeCounter] = useState(0);
 
   const onTypingDone = () => {
     setTypeCounter(typeCounter + 1);
   };
+  const showTyping = !isDev();
 
-  return (
-    <>
-      <PageHeader title="Hero" hidden />
-      <div className="mt-8 flex max-w-screen-xl items-start justify-between w-full">
-        <div className="z-10 w-full max-w-xl px-1 xl:px-0">
-          <Typist
-            cursor={{ hideWhenDone: true, show: false }}
-            key={1}
-            onTypingDone={onTypingDone}
-            avgTypingDelay={35}
-          >
-            <Code weight="bold" variant="soft" size="9">
-              hello, world.
-            </Code>
-            <Typist.Delay ms={500} />
-          </Typist>
-          {typeCounter > 0 && (
-            <Typist
-              cursor={{ hideWhenDone: true, show: false }}
-              key={2}
-              onTypingDone={onTypingDone}
-              avgTypingDelay={35}
-            >
-              <Heading className="text-left font-thin text-4xl">
-                My name is{" "}
-                <Link href="/about" className="font-bold text-accent-color">
-                  Michael Cohen
-                </Link>
-                .
-              </Heading>
-              <Typist.Delay ms={500} />
-            </Typist>
-          )}
-          {typeCounter > 1 && (
-            <Typist
-              cursor={{ hideWhenDone: true, show: false }}
-              key={2}
-              onTypingDone={onTypingDone}
-              avgTypingDelay={35}
-            >
-              <Heading className="text-left font-thin text-4xl">
-                I&apos;m a software engineer.
-              </Heading>
-              <Typist.Delay ms={500} />
-            </Typist>
-          )}
-        </div>
-        <Fade
-          right={true}
-          bottom={false}
-          duration={1500}
-          distance="30px"
-          when={typeCounter > 2}
-        >
-          <Image
-            src="/profile.jpg"
-            alt="Michael Profile image"
-            width="400"
-            height="400"
-            className="grayscale mr-2 rounded-6 hover:grayscale-0 drop-shadow-2xl"
-          />
-        </Fade>
-      </div>
-    </>
-  );
-}
-
-function StaticHero() {
   return (
     <>
       <PageHeader title="Hero" hidden />
       <Grid columns="2" width="100%" justify="between" mt="6">
         <Flex direction="column" justify="start" width="max-content">
-          <Code weight="bold" variant="soft" size="9">
-            hello, world.
-          </Code>
-          <Heading className="text-left font-thin text-4xl ml-4">
-            My name is{" "}
-            <Link href="/about" className="text-accent-color">
-              <Strong>Michael Cohen</Strong>
-            </Link>
-            .
-          </Heading>
-          <Heading className="text-left font-thin text-4xl ml-4">
-            I&apos;m a software engineer.
-          </Heading>
-          <Typist.Delay ms={500} />
+          {showTyping ? firstTypingBlock(onTypingDone) : firstTextBlock}
+          {showTyping
+            ? secondTypingBlock(typeCounter, onTypingDone)
+            : secondTextBlock}
+          {showTyping
+            ? thirdTypingBlock(typeCounter, onTypingDone)
+            : thirdTextBlock}
         </Flex>
         <Flex justify="end">
-          <Fade right={true} bottom={false} duration={1500} distance="30px">
+          <Fade
+            right={true}
+            bottom={false}
+            duration={1500}
+            distance="30px"
+            when={typeCounter > 2 || !showTyping}
+          >
             <Image
               src="/profile.jpg"
               alt="Michael Profile image"
@@ -121,8 +119,4 @@ function StaticHero() {
       </Grid>
     </>
   );
-}
-
-export default function Hero() {
-  return isDev() ? <StaticHero /> : <TypingHero />;
 }
