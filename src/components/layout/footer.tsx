abd2c04891383a70a90d8e43cc2f0d39f8e2514e
Michael Cohen <micohen13@gmail.com>
2023-11-20 04:03:42 -0500

    fix footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="absolute w-full py-5 text-center dark:bg-slate-800 bg-slate-50">
+    <div className="absolute w-full py-5 text-center">
       <p className="text-gray-500">
         A website by{" "}
         <a
