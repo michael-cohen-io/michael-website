2ea926c86f0432591aa63000f35e979317750de6
Michael Cohen <micohen13@gmail.com>
2023-11-20 16:03:18 -0500

    declarations

--- decs.d.ts
+++ decs.d.ts
@@ -1,4 +1,5 @@
 declare module "@meronex/icons/mdc/MdcHexagonSlice6";
 declare module "@meronex/icons/mdc/MdcTelevisionClassic";
 declare module "@meronex/icons/mdc/MdcFileDocumentOutline";
+declare module "@meronex/icons/ri/RiStackFill";
 declare module "react-reveal/Fade";
