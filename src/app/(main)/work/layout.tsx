d1893d1264cb6697f95458789ecf14c4af30aaaa
Michael Cohen <micohen13@gmail.com>
2023-11-20 00:18:03 -0500

    remove modal

--- src/app/(main)/work/layout.tsx
+++ src/app/(main)/work/layout.tsx
@@ -1,16 +1,11 @@
-import Link from "next/link";
 import React from "react";
 
-export default function Layout(props: {
-  children: React.ReactNode;
-  modal: React.ReactNode;
-}) {
+export default function Layout(props: { children: React.ReactNode }) {
   return (
     <>
       <div className="mx-5 flex max-w-screen-xl items-center justify-center w-full my-4">
         {props.children}
       </div>
-      {props.modal}
     </>
   );
 }
