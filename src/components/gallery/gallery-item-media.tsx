4ee315b252b384502be874b0657799f752223f38
Michael Cohen <micohen13@gmail.com>
2024-01-25 13:06:35 -0500

    lint-fix

--- src/components/gallery/gallery-item-media.tsx
+++ src/components/gallery/gallery-item-media.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { NFT, NFTMedia } from "@/lib/types";
-import Heading from "../typography/heading";
 
 const CardItemImage = ({ item, media }: { item: NFT; media: NFTMedia }) => {
   return (
